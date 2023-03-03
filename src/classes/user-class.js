d1ffbed51f172aec16227b2d40c9a3e583414b0e
Quynh Luu <quynhtlluu@gmail.com>
2023-03-03 12:32:07 -0800

    Write method to generate available rooms

--- src/classes/user-class.js
+++ src/classes/user-class.js
@@ -7,4 +7,19 @@ class User {
         this.roomsBooked = []
         this.expenses = 0
     }
+    renderAvailableRooms(date, bookingData, roomData) {
+        let numberOfBookedRooms = bookingData.reduce((arr, booking) => {
+          if(booking.date === date) {
+            arr.push(booking.roomNumber)
+          }
+          return arr
+        }, [])
+        let availableRooms = roomData.reduce((arr, room) => {
+          if(!numberOfBookedRooms.includes(room.number)) {
+            arr.push(room)
+          }
+          return arr
+        }, [])
+        return availableRooms
+      }
 }
