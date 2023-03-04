5c34549553a48e2f4425ba5bd47ed6fbdf5d24ab
Quynh Luu <quynhtlluu@gmail.com>
2023-03-03 16:29:11 -0800

    Add tests to check for other properties

--- test/rooms-test.js
+++ test/rooms-test.js
@@ -20,4 +20,28 @@ describe('rooms', () => {
     it('should be an instance of Bookings', () => {
         expect(room1).to.be.an.instanceof(Rooms)
     })
+
+    it('should have a bed size', () => {
+        expect(room1.bedSize).to.equal("queen")
+    })
+
+    it('should clarify if the room has a bidet', () => {
+        expect(room1.hasBidet).to.equal(true)
+    })
+
+    it('should have a cost per night', () => {
+        expect(room1.costPerNight).to.equal(358.4)
+    })
+
+    it('should have a number of beds', () => {
+        expect(room1.numBeds).to.equal(1)
+    })
+
+    it('should have a room number', () => {
+        expect(room1.roomNumber).to.equal(1)
+    })
+
+    it('should have a type of room', () => {
+        expect(room1.roomType).to.equal("residential suite")
+    })
 })
