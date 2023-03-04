6faae4ac8fd704eee5896a2076d51221b63df151
Quynh Luu <quynhtlluu@gmail.com>
2023-03-03 16:22:16 -0800

    Add initial describe block for rooms

---
+++ test/rooms-test.js
@@ -0,0 +1,15 @@
+describe('rooms', () => {
+    let room1;
+    beforeEach(() => {
+        const roomSample = {
+            bedSize: "queen",
+            bidet: true,
+            costPerNight: 358.4,
+            numBeds: 1,
+            number: 1,
+            roomType: "residential suite"
+            }
+ 
+        room1 = new Rooms(roomSample)
+    })
+})
